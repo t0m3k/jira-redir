7b88d57f3f2d444a9cbadd73c9639fd851296f27
Tomasz Tracz <t@tracz.me>
2023-04-28 23:45:48 +0100

    Improve ui

--- src/components/ui/Keycap.tsx
+++ src/components/ui/Keycap.tsx
@@ -1,7 +1,7 @@
 export default function Keycap({ children }: { children: React.ReactNode }) {
   return (
     <div className="inset-y-0 right-0 flex">
-      <kbd className="m-1 inline-flex items-center justify-center rounded border border-gray-200 p-1.5 font-sans text-sm font-bold text-gray-200">
+      <kbd className="m-1 inline-flex items-center justify-center rounded-lg border border-gray-200 px-1.5 py-1 font-sans font-bold text-gray-200">
         {children}
       </kbd>
     </div>
